e2ce3c3e12f11e40c37c767088d39b67dfa3c887
gkvartskhava <gio.kvartskhava@gmail.com>
2025-02-27 17:32:44 +0100

    startupp

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,16 @@
 import requests
 
-endpoint = 'https://httpbin.org/'
+
+endpoint = "https://httpbin.org/status/200"
+endpoint = 'https://httpbin.org/anything'
+endpoint = 'https://httpbin.org/status/200'
+
 
 
 get_response = requests.get(endpoint)
 
 print(get_response.text)
+
+# print(get_response.json())
+# HTTP request  => HTML
+# REST API HTTP REQUEST  => JSON

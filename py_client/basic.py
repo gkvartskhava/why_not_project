1e25f263912f54658f540fbedcc6a267af6a4bdd
gkvartskhava <gio.kvartskhava@gmail.com>
2025-03-12 21:57:15 +0100

    startupp

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,13 +3,13 @@ import requests
 
 endpoint = "https://httpbin.org/status/200"
 # endpoint = 'https://httpbin.org/anything'
-# endpoint = 'https://httpbin.org/status/200'
+endpoint = 'http://127.0.0.1:8000/'
+
 
 
 
 get_response = requests.get(endpoint, json={'query':'Hello world'})
-
-# print(get_response.text)
+print(get_response.text)
 
 # print(get_response.json())
 # HTTP request  => HTML

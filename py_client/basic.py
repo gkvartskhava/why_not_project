2c19066ed945405cac5002d5b109a993e7d701e8
gkvartskhava <gio.kvartskhava@gmail.com>
2025-03-30 18:34:36 +0200

    startupp

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 
 endpoint = "https://httpbin.org/status/200"
 # endpoint = 'https://httpbin.org/anything'
-endpoint = 'http://127.0.0.1:8000/'
+endpoint = 'http://localhost:8000/'
 
 
 

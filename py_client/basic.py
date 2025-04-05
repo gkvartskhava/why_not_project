f388b31ac55523442ce328b08bd8836ef05b6983
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-05 19:24:00 +0400

    setting up working space

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,18 +1,20 @@
 import requests
 
 
-endpoint = "https://httpbin.org/status/200"
+#endpoint = "https://httpbin.org/status/200"
 # endpoint = 'https://httpbin.org/anything'
-endpoint = 'http://localhost:8000/'
+endpoint = 'http://localhost:8000/api/'
 
 
 
 
-get_response = requests.get(endpoint, json={'query':'Hello world'})
-print(get_response.text)
+get_response = requests.get(endpoint, params={'abc':123}, json={'query':'Hello world'})
 
-# print(get_response.json())
+
+# print(get_response.text)
+# print(get_response.status_code)
+
+print(get_response.json())
 # HTTP request  => HTML
 # REST API HTTP REQUEST  => JSON
 
-print(get_response.status_code)

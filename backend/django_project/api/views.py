6191470c359b3ca03d8ef5f9084b68c10d052aef
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-05 19:48:05 +0400

    new goal for project

--- backend/django_project/api/views.py
+++ backend/django_project/api/views.py
@@ -1,5 +1,27 @@
 from django.http import JsonResponse
+import json
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"Hi there, hello world"})
+    # request --> HttpRequest --> django
+    # request.body
+    print(request.GET)
+    print(request.POST)
+
+    data = {}
+    body = request.body
+    try:
+        data=json.loads(body)
+    except:
+        pass
+    
+    print(data)
+    # print(body)
+    
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers) # request.META
+    data['content_type'] = request.content_type
+    
+
+    
+    return JsonResponse(data)

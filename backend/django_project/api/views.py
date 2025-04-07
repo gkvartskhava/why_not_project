0ddff8681a69bf443fc56e302752463cc95e8088
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-07 16:22:04 +0400

    decorating view

--- backend/django_project/api/views.py
+++ backend/django_project/api/views.py
@@ -1,8 +1,12 @@
-from django.http import JsonResponse,HttpResponse
+# from django.http import JsonResponse,HttpResponse
 import json
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     model_data =Product.objects.all().order_by('?').first()
     data = {}
@@ -11,5 +15,5 @@ def api_home(request, *args, **kwargs):
         data = model_to_dict(model_data, fields=['title', 'content', 'price'])
     else:
         pass
-    return JsonResponse(data)
+    return Response(data)
     # return HttpResponse(data, headers = {"content-type": "aplication/json"})

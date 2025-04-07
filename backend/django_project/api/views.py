06ad7d10b86610d885269d5db708d19802f3b8b0
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-07 21:37:54 +0400

    added serializer, with discount feature

--- backend/django_project/api/views.py
+++ backend/django_project/api/views.py
@@ -4,16 +4,17 @@ from products.models import Product
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from products.serializers import ProductSerializer
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data =Product.objects.all().order_by('?').first()
+    instance =Product.objects.all().order_by('?').first()
     data = {}
 
-    if model_data:
-        data = model_to_dict(model_data, fields=['title', 'content', 'price'])
-    else:
-        pass
-    return Response(data)
+    if instance:
+        data = ProductSerializer(instance).data
+    #     data = model_to_dict(instance, fields=['id','title', 'content', 'price','sale_price'])
+    # else:
+    #     pass
+        return Response(data)
     # return HttpResponse(data, headers = {"content-type": "aplication/json"})

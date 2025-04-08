e3b5f7049c92c81bc57f2567bfd38d03e0ef8f00
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-08 23:16:44 +0400

    added serializer

--- backend/django_project/api/urls.py
+++ backend/django_project/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('',views.api_home)
-}
+]
--- py_client/basic.py
+++ py_client/basic.py
@@ -8,11 +8,11 @@ endpoint = 'http://localhost:8000/api/'
 
 
 
-get_response = requests.get(endpoint, params={'abc':123}, json={'query':'Hello world'})
+get_response = requests.post(endpoint, params={'abc':123}, json={'query':'Hello world'})
 
 
 print(get_response.text)
-print(get_response.headers)
+# print(get_response.headers)
 #print(get_response.status_code)
 
 # print(get_response.json())

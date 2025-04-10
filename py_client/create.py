6a17cd979396a50c69ef5ba20f484e8c2bc3a53a
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-10 22:56:45 +0400

    created product detail.py

---
+++ py_client/create.py
@@ -0,0 +1,14 @@
+import requests
+
+endpoint = 'http://localhost:8000/api/products/'
+
+
+data = {
+    "title" : "this field is done",
+    "price" : 10.22
+}
+
+
+get_response = requests.post(endpoint, json=data)
+
+print(get_response.json())
--- py_client/detail.py
+++ py_client/detail.py
@@ -2,10 +2,6 @@ import requests
 
 endpoint = 'http://localhost:8000/api/products/2/'
 
-
-
-
 get_response = requests.get(endpoint)
-
 
 print(get_response.json())

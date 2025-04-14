b6e04b66e39209a2a662351f94e4842cc51287e3
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-14 22:06:13 +0400

    editing urls

--- backend/django_project/products/urls.py
+++ backend/django_project/products/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.ProdductMixinView.as_view()),
-    path('<int:pk>/update/', views.ProdductMixinView.as_view()),
-    path('<int:pk>/delete/', views.ProdductMixinView.as_view()),
-    path('<int:pk>/', views.ProdductMixinView.as_view())
+    path('', views.ProductListCreateApiView.as_view()),
+    path('<int:pk>/update/', views.ProductUpdateApiView.as_view()),
+    path('<int:pk>/delete/', views.ProductDeleteApiView.as_view()),
+    path('<int:pk>/', views.ProductDetailApiView.as_view())
 
 ]
 
--- backend/django_project/products/views.py
+++ backend/django_project/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics,mixins
+from rest_framework import authentication,generics,mixins,permissions
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -12,6 +12,9 @@ from django.shortcuts import get_object_or_404
 class ProductListCreateApiView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)

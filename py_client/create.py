7a5310c4a02cffbb979862f497fcce1808e9f59b
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-15 20:43:53 +0400

    authentication added

--- py_client/create.py
+++ py_client/create.py
@@ -2,14 +2,17 @@ import requests
 
 endpoint = 'http://localhost:8000/api/products/'
 
+headers = {
+    "Authorization" : "Bearer 3203034034271981d4461014ea0339dadf703778"
+}
 
 data = {
-    "title" : "deep thaughts",
+    "title" : "deep thaughts with george",
     "content": "to be or not to be",
     "price" : 500000
 }
 
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers = headers)
 
 print(get_response.json())

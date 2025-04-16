517ba8d9c6aff635a4080ed909bf1e79321e3efd
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-16 18:00:57 +0400

    editing serializers

--- backend/django_project/products/serializers.py
+++ backend/django_project/products/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 from .models import Product
-
+from rest_framework.reverse import reverse 
 
 class ProductSerializer(serializers.ModelSerializer):
 
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name= 'product-detail',lookup_field="pk",)
 
     class Meta:
         model = Product
-        fields = ["id",'title','content','price','sale_price','my_discount']
+        fields = ['url','edit_url',"pk",'title','content','price','sale_price','my_discount']
 
 
     def get_my_discount(self,obj):
         return obj.get_discount()
+    
+    def get_edit_url(self,obj):
+
+        request = self.context.get('request')
+        if request is None:
+            return None
+
+        return reverse('product-edit', kwargs={'pk': obj.pk}, request=request)
--- backend/django_project/products/urls.py
+++ backend/django_project/products/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.ProductListCreateApiView.as_view()),
-    path('<int:pk>/update/', views.ProductUpdateApiView.as_view()),
+    path('', views.ProductListCreateApiView.as_view(),name = 'product-list'),
+    path('<int:pk>/update/', views.ProductUpdateApiView.as_view(),name = 'product-edit'),
     path('<int:pk>/delete/', views.ProductDeleteApiView.as_view()),
-    path('<int:pk>/', views.ProductDetailApiView.as_view())
+    path('<int:pk>/', views.ProductDetailApiView.as_view(),name = 'product-detail')
 
 ]
 

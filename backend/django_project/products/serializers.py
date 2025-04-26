02cc615bdd75743c4a4eb28e2ce64e14e508f64e
gkvartskhava <gio.kvartskhava@gmail.com>
2025-04-26 15:30:21 +0400

    added validators for models

--- backend/django_project/products/serializers.py
+++ backend/django_project/products/serializers.py
@@ -1,18 +1,29 @@
 from rest_framework import serializers
 from .models import Product
-from rest_framework.reverse import reverse 
+from rest_framework.reverse import reverse
+
+from .validators import validate_title
 
 class ProductSerializer(serializers.ModelSerializer):
 
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
-    url = serializers.HyperlinkedIdentityField(view_name= 'product-detail',lookup_field="pk",)
-    email = serializers.EmailField(write_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name= 'product-detail',lookup_field="pk")
+
+    title = serializers.CharField(validators=[])
+    # email = serializers.EmailField(write_only=True)
 
     class Meta:
         model = Product
-        fields = ['email','url','edit_url',"pk",'title','content','price','sale_price','my_discount']
+        fields = ['url','edit_url',"pk",'title','content','price','sale_price','my_discount']  # 'email'
 
+
+
+    # def validate_title(self, value):
+    #     qs = Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f'{value} is already a product name.')
+    #     return value
 
     # def create(self, validated_data):
     #     # return Product.objects.create(**validated_data)
@@ -26,8 +37,7 @@ class ProductSerializer(serializers.ModelSerializer):
     #     instance.title = validated_data.get('title')
     #     return super().update(instance,validated_data)
 
-    def get_my_discount(self,obj):
-        return obj.get_discount()
+   
 
     def get_edit_url(self,obj):
 
@@ -36,3 +46,6 @@ class ProductSerializer(serializers.ModelSerializer):
             return None
 
         return reverse('product-edit', kwargs={'pk': obj.pk}, request=request)
+    
+    def get_my_discount(self,obj):
+        return obj.get_discount()

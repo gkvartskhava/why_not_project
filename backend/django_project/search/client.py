e33eb0fe46e76f6976fd3c84dbe28f9620953cbb
gkvartskhava <gio.kvartskhava@gmail.com>
2025-05-14 00:18:29 +0400

    added new search

--- backend/django_project/search/client.py
+++ backend/django_project/search/client.py
@@ -4,9 +4,13 @@ from algoliasearch_django import algolia_engine
 
 import urllib.parse
 
+
 def get_client():
-    client = SearchClientSync("UO647ZSSUT", "7d4b34609d91ccbb9a947881d462495b")
-    return client
+    return algolia_engine.client
+
+# def get_client():
+#     client = SearchClientSync("UO647ZSSUT", "7d4b34609d91ccbb9a947881d462495b")
+#     return client
 
 # def get_index(index_name='gio_Product'):
 #     client = get_client()
@@ -58,6 +62,42 @@ def get_index(index_name = 'gio_Product'):
     return index
 
 def perform_search(query, **kwargs):
-    index = get_index()
-    results = index.search(query)
-    return results
+    client = get_client()
+    tag_filters = kwargs.pop("tags", None)
+    facet_filters = [f"{k}:{v}" for k, v in kwargs.items() if v]
+
+    raw_params = {
+        "query": query
+    }
+
+    if tag_filters:
+        if isinstance(tag_filters, list):
+            raw_params["tagFilters"] = ",".join(tag_filters)
+        else:
+            raw_params["tagFilters"] = tag_filters
+
+    if facet_filters:
+        raw_params["facetFilters"] = facet_filters
+
+    # Convert raw_params to URL-encoded string
+    encoded_params = urllib.parse.urlencode(raw_params, doseq=True)
+
+    search_method_params = {
+        "requests": [
+            {
+                "indexName": "gio_Product",
+                "params": encoded_params
+            }
+        ]
+    }
+
+    result = client.search(search_method_params)
+    return result
+#    
+
+
+
+# def perform_search(query, **kwargs):
+#     index = get_index()
+#     results = index.search(query)
+#     return results

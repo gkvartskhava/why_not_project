4c3781f761ede9666ee3e7e4fe716cd6f6838bd4
gkvartskhava <gio.kvartskhava@gmail.com>
2025-05-19 14:17:42 +0400

    getting some errors

--- client/client.js
+++ client/client.js
@@ -1,1 +1,32 @@
-const LoginForm = document.getElementById("login-form");
+
+
+const loginForm = document.getElementById("login-form");
+const baseEndpoint = "http://localhost:8000/api";
+
+if(loginForm){
+    loginForm.addEventListener("submit",print)
+}
+
+function print(event){
+    console.log("Hello World");
+}
+
+function handleLogin(event){
+    event.preventDefault();
+    console.log(event);
+    
+    const loginEndpoint = `${baseEndpoint}/token/`;
+    let loginFormData = new FormData(loginForm);
+    let loginObjectData = Object.fromEntries(loginFormData);
+    console.log(loginObjectData)
+    const options = {
+        method: 'POST',
+        headers : {
+            'Content-Type': 'application/json',
+        },
+        body:''
+    }
+    fetch(loginEndpoint,options)
+
+}
+

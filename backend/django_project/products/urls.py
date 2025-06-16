8b258b4e5cd19ae451f33a4880887142f02f2def
gkvartskhava <gio.kvartskhava@gmail.com>
2025-06-16 23:09:36 +0400

    edited .env

--- backend/django_project/products/urls.py
+++ backend/django_project/products/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.ProductListCreateApiView.as_view(),name = 'product-list'),
-    path('<int:pk>/update/', views.ProductUpdateApiView.as_view(),name = 'product-edit'),
-    path('<int:pk>/delete/', views.ProductDeleteApiView.as_view()),
-    path('<int:pk>/', views.ProductDetailApiView.as_view(),name = 'product-detail')
+    path('', views.ProdductMixinView.as_view(),name = 'product-list'),
+    path('<int:pk>/update/', views.ProdductMixinView.as_view(),name = 'product-edit'),
+    path('<int:pk>/delete/', views.ProdductMixinView.as_view()),
+    path('<int:pk>/', views.ProdductMixinView.as_view(),name = 'product-detail')
 
 ]
 
